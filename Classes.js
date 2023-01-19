302c1f5e5055b023593b99c1a5535f051b85ec1f
alex <taloco.ale@gmail.com>
2023-01-19 17:03:41 -0300

    dia 19

---
+++ Classes.js
@@ -0,0 +1,12 @@
+class Carro {
+    constructor (marca,cor,ano){
+
+        this.marca = marca;
+        this.cor = cor;
+        this.ano = ano;
+    }
+}
+
+let carro= new Carro ("ford","amarelo",89);
+
+console.log (carro);
---
+++ get e set.js
@@ -0,0 +1,10 @@
+let arr = [1,4,6,8,9];
+let nomes = ["Brian","chaves","jaspion"];
+let bool = [true,false,true];
+
+let misturado = [4 , "chaves",true];
+
+console.log (arr);
+console.log (nomes);
+console.log (bool);
+console.log (misturado);
